6d89273b9b3e0de36c5737742e4d8cbe1af83043
Filip <92389794+w60846@users.noreply.github.com>
2022-11-12 00:55:26 +0100

    change database

--- src/main/java/com/example/demo/service/ShopService.java
+++ src/main/java/com/example/demo/service/ShopService.java
@@ -90,22 +90,22 @@ public class ShopService {
     }
 
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void fillDB() {
-        productRepo.save(new Product(1L,"Apple", ProductType.FRUITS, new BigDecimal(2)));
-        productRepo.save(new Product(2L,"Orange", ProductType.FRUITS, new BigDecimal(3)));
-        productRepo.save(new Product(3L,"Banana", ProductType.FRUITS, new BigDecimal("4.4")));
-        productRepo.save(new Product(4L,"Potato", ProductType.VEGETABLES, new BigDecimal("1.2")));
-        productRepo.save(new Product(5L,"Tomato", ProductType.VEGETABLES, new BigDecimal(7)));
-        productRepo.save(new Product(6L,"Onion", ProductType.VEGETABLES, new BigDecimal("1.7")));
-        productRepo.save(new Product(7L,"Milk", ProductType.DAIRY, new BigDecimal("2.7")));
-        productRepo.save(new Product(8L,"Cheese", ProductType.DAIRY, new BigDecimal("20.5")));
-        productRepo.save(new Product(9L,"Butter", ProductType.DAIRY, new BigDecimal(7)));
-        productRepo.save(new Product(10L,"Pork", ProductType.MEAT, new BigDecimal(16)));
-        productRepo.save(new Product(11L,"Steak", ProductType.MEAT, new BigDecimal(50)));
-        productRepo.save(new Product(12L,"Bread", ProductType.GRAINS, new BigDecimal(5)));
-        productRepo.save(new Product(13L,"Cereals", ProductType.GRAINS,new BigDecimal(8)));
-    }
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void fillDB() {
+//        productRepo.save(new Product(1L,"Apple", ProductType.FRUITS, new BigDecimal(2)));
+//        productRepo.save(new Product(2L,"Orange", ProductType.FRUITS, new BigDecimal(3)));
+//        productRepo.save(new Product(3L,"Banana", ProductType.FRUITS, new BigDecimal("4.4")));
+//        productRepo.save(new Product(4L,"Potato", ProductType.VEGETABLES, new BigDecimal("1.2")));
+//        productRepo.save(new Product(5L,"Tomato", ProductType.VEGETABLES, new BigDecimal(7)));
+//        productRepo.save(new Product(6L,"Onion", ProductType.VEGETABLES, new BigDecimal("1.7")));
+//        productRepo.save(new Product(7L,"Milk", ProductType.DAIRY, new BigDecimal("2.7")));
+//        productRepo.save(new Product(8L,"Cheese", ProductType.DAIRY, new BigDecimal("20.5")));
+//        productRepo.save(new Product(9L,"Butter", ProductType.DAIRY, new BigDecimal(7)));
+//        productRepo.save(new Product(10L,"Pork", ProductType.MEAT, new BigDecimal(16)));
+//        productRepo.save(new Product(11L,"Steak", ProductType.MEAT, new BigDecimal(50)));
+//        productRepo.save(new Product(12L,"Bread", ProductType.GRAINS, new BigDecimal(5)));
+//        productRepo.save(new Product(13L,"Cereals", ProductType.GRAINS,new BigDecimal(8)));
+//    }
 
 
 }
